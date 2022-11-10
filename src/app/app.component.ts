5ff52fe82798ea7f6365b8681e9a146bb0740521
RDJ2000 <musaibshaikh015@gmail.com>
2022-11-10 14:48:26 +0530

    task completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { DataService } from './data.service';
+DataService
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo';
+  fetchedData:any
+  constructor(private _dataService: DataService){
+
+  }
+  ngOnInit() {
+    this._dataService.getData().subscribe((data) => {
+      console.log(data);
+      this.fetchedData=data;
+    })
+   
+    
+  }
+  determineColor(num:number){
+    if(num>0 && num<3){
+      return 'red'
+    }
+    else if(num>3 && num<10){
+      return 'yellow'
+    }
+    else if(num>10){
+      return 'green'
+    }
+    else{
+      return 'default'
+    }
+  }
 }
